db32710a9d4178567de08f959528322619d7f9fc
FrancescoVenanti <francesco.venanti@gmail.com>
2025-05-27 21:28:36 +0200

    aaA

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,21 @@
+
+import fs from 'fs/promises';
+import path from 'path';
+
+export async function getMd(): Promise<string[]> {
+  const files = await getFiles();
+
+  const contents = await Promise.all(
+    files.map(async (f) => {
+      const filePath = path.join(process.cwd(), 'src/content', f);
+      return fs.readFile(filePath, 'utf8');
+    })
+  );
+
+  return contents; 
+}
+
+async function getFiles(): Promise<string[]> {
+  const files = await fs.readdir('src/content');
+  return files;
+}
