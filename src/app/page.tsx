e9a2f4947a03b54f07d83ef88f6d6f9358785d8a
FrancescoVenanti <francesco.venant@gmail.com>
2025-05-30 14:06:33 +0200

    matriciana

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
-import { Tokenizer } from "./core/tokenizer";
+import { Token, Tokenizer } from "./core/tokenizer";
 import { getMd } from "./utils/utils";
 import ShowToken from "@/components/show_token";
 
 export default async function Home() {
   const tokenizer = new Tokenizer();
   const rawMd = await getMd();
-  const content = tokenizer.tokenize(rawMd.join());
+  const content: Token[][] = rawMd.map((r) => tokenizer.tokenize(r));
 
   return (
     <main className="p-8">
       <h1 className="text-xl font-bold mb-4">Markdown Content</h1>
-      <ShowToken content={content} />
+      <p>{rawMd.join(" - - - - ")}</p>
+      {content.map((c, index) => (
+        <ShowToken content={c} key={index} />
+      ))}
     </main>
   );
 }
---
+++ src/content/test2.md
@@ -0,0 +1,3 @@
+# assass
+
+- aaa \*
